f60e016e70f0ff44257cb33db9e494e56296be6b
GhaziRiyadh <ghaziriyadh2@gmail.com>
2023-02-04 02:22:31 +0300

    edit custom render component for django and add icons

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,11 @@
 from rest_framework import routers
 from django.urls import path, include
-from user.views import index
+from user.views import index, word
 
 router = routers.DefaultRouter()
 # router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path('index/', index, name='index'),
+    path('word/', word, name='word')
 ] + router.urls
--- user/views.py
+++ user/views.py
@@ -9,11 +9,26 @@ def index(request):
     return render(request, 'pages/dashboard.html',context=context)
 
 
+def word(request):
+    context = {
+        'title': 'Word',
+        'side_bar_urls': get_urls(),
+    }
+    return render(request, 'pages/word.html', context=context)
+
+
 def get_urls():
     return {
         'index': {
             'url': '/user/index/',
-            'icon': 'fa fa-dashboard',
+            'icon': 'icons/dashboard/side_bar/dashboard-gray.svg',
+            'active_icon': 'icons/dashboard/side_bar/dashboard-white.svg',
             'title': 'Dashboard',
+        },
+        'word': {
+            'url': '/user/word/',
+            'icon': 'icons/dashboard/side_bar/book-gray.svg',
+            'active_icon': 'icons/dashboard/side_bar/book-white.svg',
+            'title': 'Word',
         },
     }
